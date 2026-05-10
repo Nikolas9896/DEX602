7ea4084de2a19687089c26c3ccda63bf8370c266
Elyas M. Kosari <elyas.kosari@gmail.com>
2026-05-10 12:37:37 +0000

    Exercise 2-5 Completed

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -10,4 +10,8 @@ export default class StudentTile extends LightningElement {
 		return this.isSelected ? "tile selected " : "tile";
 	}
 	
+	handleStudentClick(){
+		alert(this.student.Name);
+	}
+	
 }
