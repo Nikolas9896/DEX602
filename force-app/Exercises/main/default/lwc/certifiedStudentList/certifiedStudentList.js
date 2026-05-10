fdb73ed8d1a9a48a072db6c1d1826d2cb51fb0bf
Elyas M. Kosari <elyas.kosari@gmail.com>
2026-05-10 12:37:38 +0000

    Exercise 5-4 Feature Not Available

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -1,95 +1,101 @@
-import { LightningElement, api, wire } from 'lwc';
-import getCertifiedStudents from '@salesforce/apex/CertifiedStudentList.getCertifiedStudents';
-import deleteStudentCertification from
-'@salesforce/apex/CertifiedStudentList.deleteStudentCertification';
-import { refreshApex } from '@salesforce/apex';
+import { LightningElement, api, wire } from "lwc";
+import getCertifiedStudents from "@salesforce/apex/CertifiedStudentList.getCertifiedStudents";
+import deleteStudentCertification from "@salesforce/apex/CertifiedStudentList.deleteStudentCertification";
+import { refreshApex } from "@salesforce/apex";
+import Utils from "c/utils";
 
 export default class CertifiedStudentList extends LightningElement {
+  @api certificationId = 0;
+  @api certificationName = "";
+  certifiedStudents;
+  btnGroupDisabled = true;
+  error;
+  _wiredStudentResult;
 
-    @api certificationId = 0;
-    @api certificationName = '';
-    certifiedStudents;
-    btnGroupDisabled = true;
-    error;
-    _wiredStudentResult;
-    
-    @wire(getCertifiedStudents, {certificationId: '$certificationId'})
-    wired_getCertifiedStudents(result) {
-        this._wiredStudentResult = result;
-        this.certifiedStudents = [];
-        if (result.data) {
-            this.certifiedStudents = result.data.map(student => ({
-                certificationHeldId: student.Id,
-                contactId: student.Certified_Professional__r.Id,
-                name: student.Certified_Professional__r.Name,
-                date: student.Date_Achieved__c,
-                email: student.Certified_Professional__r.Email,
-                phone: student.Certified_Professional__r.Phone
-            }));
-        } else if (result.error) {
-            this.error = result.error;
-        }
+  @wire(getCertifiedStudents, { certificationId: "$certificationId" })
+  wired_getCertifiedStudents(result) {
+    this._wiredStudentResult = result;
+    this.certifiedStudents = [];
+    if (result.data) {
+      this.certifiedStudents = result.data.map((student) => ({
+        certificationHeldId: student.Id,
+        contactId: student.Certified_Professional__r.Id,
+        name: student.Certified_Professional__r.Name,
+        date: student.Date_Achieved__c,
+        email: student.Certified_Professional__r.Email,
+        phone: student.Certified_Professional__r.Phone,
+      }));
+    } else if (result.error) {
+      this.error = result.error;
     }
+  }
 
-    columnConfig = [
-        {
-            label: 'Name',
-            fieldName: 'name',
-            type: 'text'
-        },
-        {
-            label: 'Date',
-            fieldName: 'date',
-            type: 'text'
-        },
-        {
-            label: 'Email',
-            fieldName: 'email',
-            type: 'email'
-        },
-        {
-            label: 'Phone',
-            fieldName: 'phone',
-            type: 'phone'
-        }
-    ];
-    
-    handleRowSelection(event) {
-        let numSelected = event.detail.selectedRows.length;
-        this.btnGroupDisabled= (numSelected===0);
-    }
+  columnConfig = [
+    {
+      label: "Name",
+      fieldName: "name",
+      type: "text",
+    },
+    {
+      label: "Date",
+      fieldName: "date",
+      type: "text",
+    },
+    {
+      label: "Email",
+      fieldName: "email",
+      type: "email",
+    },
+    {
+      label: "Phone",
+      fieldName: "phone",
+      type: "phone",
+    },
+  ];
 
-    getSelectedIDs() {
-        let datatable = this.template.querySelector('lightning-datatable');
-        let ids = datatable.getSelectedRows().map( (r) => (
-            r.certificationHeldId            
-        ));
-        return ids;
-    }
+  handleRowSelection(event) {
+    let numSelected = event.detail.selectedRows.length;
+    this.btnGroupDisabled = numSelected === 0;
+  }
 
-    handleCertActions (event) {
-        const btnClicked = event.target.getAttribute('data-btn-id');
-        switch (btnClicked) {
-            case 'btnEmail':
-                break;
-            case 'btnSendCert':
-                break;
-            case 'btnDelete':
-                this.handleDelete();
-                break;
-            default:
-                break;
-        }
+  getSelectedIDs() {
+    let datatable = this.template.querySelector("lightning-datatable");
+    let ids = datatable.getSelectedRows().map((r) => r.certificationHeldId);
+    return ids;
+  }
+
+  handleCertActions(event) {
+    const btnClicked = event.target.getAttribute("data-btn-id");
+    switch (btnClicked) {
+      case "btnEmail":
+        this.notAvailable();
+        break;
+      case "btnSendCert":
+        this.notAvailable();
+        break;
+      case "btnDelete":
+        this.handleDelete();
+        break;
+      default:
+        break;
     }
-    handleDelete() {
-        const certificationHeldIds = this.getSelectedIDs();
-        deleteStudentCertification({certificationHeldIds})
-        .then(  () => {
-            refreshApex(this._wiredStudentResult);
-        })
-        .catch(error => {
-            this.error = error;
-        });
-        
-    }
+  }
+  handleDelete() {
+    const certificationHeldIds = this.getSelectedIDs();
+    deleteStudentCertification({ certificationHeldIds })
+      .then(() => {
+        refreshApex(this._wiredStudentResult);
+      })
+      .catch((error) => {
+        this.error = error;
+      });
+  }
+
+  notAvailable() {
+    Utils.showModal(
+      "Not Available",
+      `This feature is
+		currently unavailable`,
+    );
+  }
 }
