55c111f0cc16aea98f9e387e20391c1724133636
Elyas M. Kosari <elyas.kosari@gmail.com>
2026-05-10 12:37:37 +0000

    Lab Phase 1 Completed

--- force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
+++ force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
@@ -3,7 +3,7 @@ import { LightningElement, wire } from 'lwc';
 import { getRecord, getFieldValue, getFieldDisplayValue } from 'lightning/uiRecordApi';
 import { subscribe, unsubscribe, MessageContext } from 'lightning/messageService';
 import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
-
+import { NavigationMixin } from 'lightning/navigation';
 
 import FIELD_NAME from '@salesforce/schema/Contact.Name';
 import FIELD_DESCRIPTION from '@salesforce/schema/Contact.Description';
@@ -11,13 +11,13 @@ import FIELD_EMAIL from '@salesforce/schema/Contact.Email';
 import FIELD_PHONE from '@salesforce/schema/Contact.Phone';
 const fields = [FIELD_NAME, FIELD_DESCRIPTION, FIELD_EMAIL, FIELD_PHONE];
 
-export default class StudentDetail extends LightningElement {
+export default class StudentDetail extends NavigationMixin(LightningElement) {
 
 	studentId;
 	subscription;
 
 	@wire(MessageContext) messageContext;
-	
+
 	@wire(getRecord, { recordId: '$studentId', fields })
 	wiredStudent;
 	
@@ -64,6 +64,16 @@ export default class StudentDetail extends LightningElement {
 
 	handleStudentChange(message) {
 		this.studentId = message.studentId;
+	}
+
+	handleGoToRecord(evt) {
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: this.studentId,
+				actionName: 'view'
+			},
+		});
 	}
 	
 	_getDisplayValue(data, field) {
