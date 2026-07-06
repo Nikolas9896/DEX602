52952d487a9c028c285431b87108a265012b4a50
Mykola Hlynka <nhlynka@gmail.com>
2026-07-06 13:39:25 +0000

    Exercise 2-4 Completed

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,13 @@
+import { LightningElement, api } from 'lwc';
+
+export default class StudentTile extends LightningElement {
+    @api isSelected = false;
+    @api student = {
+        Name: 'Mykola Hlynka',
+        PhotoUrl: '/services/images/photo/003B0FakePictId'
+    };
+    get tileSelected() {
+        return this.isSelected ? "tile selected" : "tile";
+    };
+
+}
